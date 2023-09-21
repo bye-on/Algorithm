e6a30d4791a1a08fbb62256265db2f86816f93e3
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2023-09-21 23:35:45 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int dp[1000001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+	}
+
+	cout << dp[n];
+}
