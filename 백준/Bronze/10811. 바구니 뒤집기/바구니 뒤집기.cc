4fbf569740c0ec87cfca8e74e5651428e1e94a2a
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2024-11-18 14:47:34 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-int basket[101];
 
 int main()
 {
 	int N, M;
 	cin >> N >> M;
 
+	vector<int> basket;
 	for (int i = 1; i <= N; i++)
 	{
-		basket[i] = i;
+		basket.push_back(i);
 	}
 
 	for (int m = 0; m < M; m++)
@@ -19,19 +19,10 @@ int main()
 		int i, j;
 		cin >> i >> j;
 
-		vector<int> v;
-		for (int a = j; a >= i; a--)
-		{
-			v.push_back(basket[a]);
-		}
-
-		for (int a = 0; a < v.size(); a++)
-		{
-			basket[i + a] = v[a];
-		}
+		reverse(basket.begin() + i - 1, basket.begin() + j);
 	}
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 0; i < basket.size(); i++)
 	{
 		cout << basket[i] << " ";
 	}
