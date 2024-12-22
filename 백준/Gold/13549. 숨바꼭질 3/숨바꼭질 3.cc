611b15349ace8f1e639171906e4ff8aa6c110139
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2024-12-22 20:37:08 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int n, k;
+int path[200001];
+int INF = 1e9;
+
+int main()
+{
+	cin >> n >> k;
+	deque<int> d;
+
+	for (int i = 0; i < 200001; i++)
+	{
+		path[i] = INF;
+	}
+
+	path[n] = 0;
+	d.push_front(n);
+
+	while (!d.empty())
+	{
+		int current = d.front();
+		d.pop_front();
+
+		if (current == k)
+		{
+			cout << path[k];
+			break;
+		}
+
+		int next = current * 2;
+		if (next < 100001 && path[next] > path[current] + 1)
+		{
+			path[next] = path[current];
+			d.push_front(next);
+		}
+
+		next = current - 1;
+		if (next >= 0 && path[next] > path[current] + 1)
+		{
+			path[next] = path[current] + 1;
+			d.push_back(next);
+		}
+		next = current + 1;
+		if (next < 100001 && path[next] > path[current] + 1)
+		{
+			path[next] = path[current] + 1;
+			d.push_back(next);
+		}
+	}
+}
