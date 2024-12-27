84a1c8696316962a0339d7fa148153d44989e71c
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2024-12-27 16:07:35 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 6132 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int map[1026][1026];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			int input;
+			cin >> input;
+			map[i][j] = input;
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			map[i][j] += map[i - 1][j] + map[i][j - 1] - map[i - 1][j - 1];
+		}
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		cout << map[x2][y2] - map[x2][y1 - 1] - map[x1 - 1][y2] + map[x1 - 1][y1 - 1] << '\n';
+	}
+}
