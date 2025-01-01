74c80972c24bc905925064195c85a63aa21ef413
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-01-01 17:21:49 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 212 ms, Memory: 22640 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+
+	vector<int> lis;
+	vector<int> path;
+
+	for (int i = 0; i < n; i++)
+	{
+		int num = v[i];
+		int index = lower_bound(lis.begin(), lis.end(), num) - lis.begin();
+		if (index == lis.size())
+		{
+			lis.push_back(num);
+			path.push_back(lis.size() - 1);
+		}
+		else
+		{
+			lis[index] = num;
+			path.push_back(index);
+		}
+	}
+
+	vector<int> result;
+	int length = lis.size() - 1;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (path[i] == length)
+		{
+			result.push_back(v[i]);
+			length--;
+		}
+	}
+
+	cout << lis.size() << '\n';
+	sort(result.begin(), result.end());
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << " ";
+	}
+}
