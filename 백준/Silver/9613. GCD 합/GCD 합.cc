a0d37da84a0b348824d07deb5b067a0294e2c137
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-02-25 16:05:49 +0900

    [Silver IV] Title: GCD 합, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.cc
+++ 백준/Silver/9613. GCD 합/GCD 합.cc
@@ -2,15 +2,36 @@
 #include <vector>
 using namespace std;
 
-long long gcd(long long a, long long b)
+long long result = 0;
+int gcd(int a, int b)
 {
 	while (b != 0)
 	{
-		long long temp = b;
+		int temp = b;
 		b = a % b;
 		a = temp;
 	}
 	return a;
+}
+
+void combination(vector<int>& comb, vector<int>& arr, int r, int index, int depth)
+{
+	if (r == 0)
+	{
+		if (comb[0] > comb[1])
+			result += gcd(comb[0], comb[1]);
+		else
+			result += gcd(comb[1], comb[0]);
+
+		return;
+	}
+
+	if (depth == arr.size())
+		return;
+
+	comb[index] = arr[depth];
+	combination(comb, arr, r - 1, index + 1, depth + 1);
+	combination(comb, arr, r, index, depth + 1);
 }
 
 int main()
@@ -23,25 +44,17 @@ int main()
 		int n;
 		cin >> n;
 		
-		vector<long long> v;
+		vector<int> v;
 		for (int i = 0; i < n; i++)
 		{
-			long long input;
+			int input;
 			cin >> input;
 			v.push_back(input);
 		}
 
-		long long result = 0;
-		for (int i = 0; i < v.size() - 1; i++)
-		{
-			for (int j = i + 1; j < v.size(); j++)
-			{
-				if (v[i] < v[j])
-					result += gcd(v[j], v[i]);
-				else
-					result += gcd(v[i], v[j]);
-			}
-		}
+		result = 0;
+		vector<int> comb(3);
+		combination(comb, v, 2, 0, 0);
 
 		cout << result << '\n';
 	}
