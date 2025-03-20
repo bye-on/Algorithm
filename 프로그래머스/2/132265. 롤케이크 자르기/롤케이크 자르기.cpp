f7a9d99052c0fc084d45f58148da853581154db5
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-03-20 09:17:28 +0900

    [level 2] Title: 롤케이크 자르기, Time: 70.59 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<int> topping) {
+    int answer = 0;
+    
+    unordered_map<int, int> left;
+    unordered_map<int, int> right;
+    
+    for(int i = 0; i < topping.size(); i++)
+    {
+        if(left.find(topping[i]) == left.end())
+            left.insert({topping[i], 1});
+        else
+            left[topping[i]]++;
+    }
+    
+    for(int i = topping.size() - 1; i >= 0; i--)
+    {
+        if(right.find(topping[i]) == right.end())
+            right.insert({topping[i], 1});
+        else
+            right[topping[i]]++;
+        
+        left[topping[i]]--;
+        if(left[topping[i]] == 0)
+            left.erase(left.find(topping[i]));
+        
+        if(left.size() == right.size())
+            answer++;
+    }
+    
+    return answer;
+}
