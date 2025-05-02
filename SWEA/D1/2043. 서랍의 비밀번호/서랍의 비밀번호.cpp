f178e7e9bd440896e693976997029bad13275dd5
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-05-02 17:35:11 +0900

    [D1] 2043: 서랍의 비밀번호

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int p, k;
+    cin >> p >> k;
+        
+    cout << p - k + 1;
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
