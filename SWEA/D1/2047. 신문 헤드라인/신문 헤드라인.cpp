60d5cf6025db893fc0d764c9b022f2c742d8e647
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-05-03 16:10:43 +0900

    [D1] 2047: 신문 헤드라인

--- SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.cpp
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.cpp
@@ -9,9 +9,13 @@ int main(int argc, char** argv)
     
     for(int i = 0; i < str.size(); i++)
     {
-        char c = toupper(str[i]);
-        cout << c;        
+        char c = str[i];
+        if(c <= 'z' && c >= 'a')
+        	c -= ('a' - 'A');
+        str[i] = c;
     }
+    
+    cout << str;
     
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
