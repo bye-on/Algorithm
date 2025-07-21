2ad3e8a8909ea1c7af42d8e09a3a0cdfbf7a47b5
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-07-21 23:18:18 +0900

    [Silver IV] 4949: 균형잡힌 세상

---
+++ 백준/Silver/4949. 균형잡힌 세상/4949.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String[] str = br.readLine().split("");
+            
+            if(str[0].equals("."))
+                break;
+            
+            Stack<String> stack = new Stack<>();
+            boolean flag = true;
+
+            for (int i = 0; i < str.length; i++) {
+                if(str[i].equals("("))
+                    stack.push("(");
+                else if(str[i].equals("["))
+                    stack.push("[");
+                
+                else if(str[i].equals(")")) {
+                    if(stack.isEmpty()) {
+                        sb.append("no").append("\n");
+                        flag = false;
+                        break;
+                    }
+                    else {
+                        if(stack.peek().equals("[")) {
+                            sb.append("no").append("\n");
+                            flag = false;
+                            break;
+                        }
+                        else if(stack.peek().equals("("))
+                            stack.pop(); 
+                        else {
+                            sb.append("no").append("\n");
+                            flag = false;
+                            break;
+                        }
+                    }                                
+                }
+                else if(str[i].equals("]")) {
+                    if(stack.isEmpty()) {
+                        sb.append("no").append("\n");
+                        flag = false;
+                        break;
+                    }
+                    else {
+                        if(stack.peek().equals("(")) {
+                            sb.append("no").append("\n");
+                            flag = false;
+                            break;
+                        }
+                        else if(stack.peek().equals("["))
+                            stack.pop(); 
+                        else {
+                            sb.append("no").append("\n");
+                            flag = false;
+                            break;
+                        }
+                    }                         
+                }
+            }
+            if(!stack.isEmpty() && flag) {
+                sb.append("no").append("\n");
+                flag = false;
+            }
+
+            if(flag)
+                sb.append("yes").append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
