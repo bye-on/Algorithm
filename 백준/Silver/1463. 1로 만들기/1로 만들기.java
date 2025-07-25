8cbf976f7d5d69c9423be1b151d19b50de61134a
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-07-25 13:34:16 +0900

    [Silver III] 1463: 1로 만들기

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[n + 1];
-        dp[1] = 0;
-        
+        dp = new int[n + 1];
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
 
