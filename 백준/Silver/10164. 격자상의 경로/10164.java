e894ebfd26c5b00c1955b80b82e433c8d22ad839
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-07-27 21:42:14 +0900

    [Silver II] 10164: 격자상의 경로

--- 백준/Silver/10164. 격자상의 경로/10164.java
+++ 백준/Silver/10164. 격자상의 경로/10164.java
@@ -10,12 +10,26 @@ public class Main {
         int m = Integer.parseInt(str[1]);
         int k = Integer.parseInt(str[2]);
 
-        int[][] dp = new int[n + 1][m + 1];
+        int[][] dp = new int[n][m];
+
+        int num = 1;
+        int y = 0;
+        int x = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+                if(num == k) {
+                    y = i;
+                    x = j;
+                }
+                num++;
+            }
+        }
+
+        for (int i = 0; i <= y; i++) {
+            for (int j = 0; j <= x; j++) {
                 if (i == 0 && j == 0)
-                    dp[i][j] = 0;
+                    dp[i][j] = 1;
                 else if (i == 0)
                     dp[i][j] = 1;
                 else if (j == 0)
@@ -25,19 +39,23 @@ public class Main {
             }
         }
 
-        int y = (k - 1) / m;
-        int x = (k % m) - 1;
+        int num1 = dp[y][x];
 
-        int cnt = 0;
-        if(k != 0) {
-            for (int i = 0; i < x; i++) {
-                cnt += dp[y + 1][i];
-            }
-            for (int i = x + 1; i < m; i++) {
-                cnt += dp[y - 1][i];
+        for (int i = y; i < n; i++) {
+            for (int j = x; j < m; j++) {
+                if (i == y && j == x)
+                    dp[i][j] = 1;
+                else if (i == y)
+                    dp[i][j] = 1;
+                else if (j == x)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        System.out.println(dp[n - 1][m - 1] - cnt);
+        int num2 = dp[n - 1][m - 1];
+
+        System.out.println(num1 * num2);
     }
 }
