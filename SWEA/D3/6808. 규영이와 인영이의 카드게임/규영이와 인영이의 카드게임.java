876f8d5063511c5e445959b69d0a8f62ceafcb2b
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-08-06 21:54:07 +0900

    [D3] 6808: 규영이와 인영이의 카드게임

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -6,16 +6,21 @@ class Solution {
     static int[] A;
     static int[] B;
     static boolean[] visited;
+    static int[] arr;
     static long resultA;
     static long resultB;
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int T = Integer.parseInt(br.readLine());
 
         for (int test_case = 1; test_case <= T; test_case++) {
-            A = new int[9];
-            B = new int[9];
+            A = new int[9]; // 규영
+            B = new int[9]; // 인영
+
+            arr = new int[9]; // 9!의 결과들
             visited = new boolean[19];
             resultA = 0;
             resultB = 0;
@@ -28,9 +33,16 @@ class Solution {
                 visited[n] = true;
             }
 
+            int index = 0;
+            for (int i = 1; i <= 18; i++) {
+                if (!visited[i])
+                    B[index++] = i;
+            }
+
             dfs(0);
-            System.out.println("#"+ test_case + " " + resultA + " " + resultB);
+            sb.append("#").append(test_case).append(" ").append(resultA).append(" ").append(resultB).append("\n");
         }
+        System.out.println(sb.toString());
     }
 
     static void dfs(int depth) {
@@ -39,13 +51,12 @@ class Solution {
             int b = 0;
 
             for (int i = 0; i < A.length; i++) {
-                if (A[i] < B[i]) {
+                if (A[i] < arr[i]) {
                     b += A[i];
-                    b += B[i];
-                }
-                else if(A[i] > B[i]) {
+                    b += arr[i];
+                } else if (A[i] > arr[i]) {
                     a += A[i];
-                    a += B[i];
+                    a += arr[i];
                 }
             }
 
@@ -57,12 +68,14 @@ class Solution {
             return;
         }
 
-        for (int i = 1; i <= 18; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                B[depth] = i;
+        for (int i = 0; i < 9; i++) {
+            int num = B[i];
+            
+            if (!visited[num]) {
+                visited[num] = true;
+                arr[depth] = num;
                 dfs(depth + 1);
-                visited[i] = false;
+                visited[num] = false;
             }
         }
     }
