71c7c1bc6a04858ad06d06aa6b1a0f5327fe355e
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-08-13 10:29:22 +0900

    [D3] 5215: 햄버거 다이어트

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,43 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-class Solution {
-    public static void main(String args[]) throws IOException {
+public class Solution {
+    static int N; static int L;
+    static boolean[] isSelected;
+    static ArrayList<int[]> ham;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
-
-        for (int test_case = 1; test_case <= T; test_case++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= t; test_case++) {
             String[] NL = br.readLine().split(" ");
-            int N = Integer.parseInt(NL[0]); // 재료의 수
-            int L = Integer.parseInt(NL[1]); // 제한 칼로리
+            N = Integer.parseInt(NL[0]);
+            L = Integer.parseInt(NL[1]);
+            
+            isSelected = new boolean[N];
+            ham = new ArrayList<>();
+            result = 0;
 
-            int[][] arr = new int[N + 1][2];
-            for (int i = 1; i <= N; i++) {
+            for (int i = 0; i < N; i++) {
                 String[] TK = br.readLine().split(" ");
-                int t = Integer.parseInt(TK[0]);
-                int k = Integer.parseInt(TK[1]);
-                arr[i][0] = t; // 점수
-                arr[i][1] = k; // 칼로리
+                int T = Integer.parseInt(TK[0]);
+                int K = Integer.parseInt(TK[1]);
+
+                ham.add(new int[] {T, K});
             }
 
-            int[][] dp = new int[N + 1][L + 1]; // [번호][칼로리], value = 점수
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= L; j++) {
-                    int value = arr[i][0];
-                    int calorie = arr[i][1];
+            subset(0);
+            sb.append("#").append(test_case).append(" ").append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-                    if (calorie > j)
-                        dp[i][j] = dp[i - 1][j];
-                    else
-                        dp[i][j] = Math.max(dp[i - 1][j], value + dp[i - 1][j - calorie]);
+    static void subset(int cnt) {
+        if(cnt == N) {
+            int score = 0; int limit = 0;
+            for (int i = 0; i < N; i++) {
+                if(isSelected[i]) {
+                    int[] tmp = ham.get(i);
+
+                    score += tmp[0];
+                    limit += tmp[1];
                 }
             }
 
-            sb.append("#").append(test_case).append(" ").append(dp[N][L]).append("\n");
+            if(score > result) {
+                if(limit <= L) {
+                    result = score;
+                }
+            }
+            return;
         }
-        System.out.println(sb.toString());
+
+        isSelected[cnt] = true;
+        subset(cnt + 1);
+
+        isSelected[cnt] = false;
+        subset(cnt + 1);
     }
 }
