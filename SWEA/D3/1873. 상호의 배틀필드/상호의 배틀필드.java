c715a02c75dbe13d2d82b3aafc76000e4c7667c5
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-08-19 17:57:52 +0900

    [D3] 1873: 상호의 배틀필드

---
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -0,0 +1,157 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    static char[][] map;
+
+    // ^ < v > (상 좌 하 우)
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static int H; static int W;
+
+    static int direction;
+    static int x;
+    static int y;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= t; test_case++) {
+            String[] HW = br.readLine().split(" ");
+            H = Integer.parseInt(HW[0]);
+            W = Integer.parseInt(HW[1]);
+
+            x = -1; y = -1;
+
+            map = new char[H][W];
+            for (int i = 0; i < H; i++) {
+                String input = br.readLine();
+                for (int j = 0; j < W; j++) {
+                    map[i][j] = input.charAt(j);
+
+                    if(map[i][j] == '^') {
+                        x = i; y = j;  
+                        direction = 0;
+                    }
+                    if(map[i][j] == '<') {
+                        x = i; y = j;
+                        direction = 1;
+                    }
+                    if(map[i][j] == 'v') {
+                        x = i; y = j;
+                        direction = 2;
+                    }
+                    if(map[i][j] == '>') {
+                        x = i; y = j;
+                        direction = 3;
+                    }
+                }
+            }
+
+            int N = Integer.parseInt(br.readLine());
+            String str = br.readLine();
+
+            for (int i = 0; i < N; i++) {
+                char c = str.charAt(i);
+                calc(c);
+            }
+
+            System.out.print("#" + test_case + " ");
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < W; j++) {
+                    System.out.print(map[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    static void calc(char c) {
+        if(c == 'S') {
+            int nx = x;
+            int ny = y;
+
+            while (true) {
+                nx += dx[direction];
+                ny += dy[direction];
+
+                if(nx < 0 || nx >= H || ny < 0 || ny >= W) break;
+                if(map[nx][ny] == '.') continue;
+                if(map[nx][ny] == '-') continue;
+                if(map[nx][ny] == '#') break;
+                if(map[nx][ny] == '*') {
+                    map[nx][ny] = '.';
+                    break;
+                }
+            }
+        }
+
+        if(c == 'U') {
+            direction = 0;
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            map[x][y] = '^';
+            if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
+                return;
+            }
+            if(map[nx][ny] == '.') {
+                map[x][y] = '.';
+                map[nx][ny] = '^';
+                x = nx; y = ny;
+            }
+        }
+
+        if(c == 'D') {
+            direction = 2;
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+            map[x][y] = 'v';
+
+            if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
+                return;
+            }
+            if(map[nx][ny] == '.') {
+                map[x][y] = '.';
+                map[nx][ny] = 'v';
+                x = nx; y = ny;
+            }
+        }
+
+        if(c == 'L') {
+            direction = 1;
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            map[x][y] = '<';
+            if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
+                return;
+            }
+            if(map[nx][ny] == '.') {
+                map[x][y] = '.';
+                map[nx][ny] = '<';
+                x = nx; y = ny;
+            }
+        }
+
+        if(c == 'R') {
+            direction = 3;
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            map[x][y] = '>';
+            if(nx < 0 || nx >= H || ny < 0 || ny >= W) {
+                return;
+            }
+            if(map[nx][ny] == '.') {
+                map[x][y] = '.';
+                map[nx][ny] = '>';
+                x = nx; y = ny;
+            }
+        }
+    }
+}
