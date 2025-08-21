27d2ac463aea041af5ddbcb2ca50196a3fa4df26
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-08-21 13:05:27 +0900

    [D4] 1227: [S/W 문제해결 기본] 7일차 - 미로2

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.cpp
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.cpp
@@ -1,76 +1,63 @@
-#include<iostream>
-#include<queue>
-using namespace std;
+import java.util.*;
 
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
-int map[100][100];
-bool visited[100][100];
+public class Solution {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] map = new int[101][101];
+    static boolean[][] visited = new boolean[101][101];
 
-bool bfs(int x, int y)
-{
-    queue<pair<int, int>> q;
-    q.push({ x,y });
-    visited[x][y] = true;
+    public static boolean bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        visited[x][y] = true;
 
-    while (!q.empty())
-    {
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            x = cur[0];
+            y = cur[1];
 
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-            if (nx < 0 || ny < 0 || nx >= 100 || ny >= 100) continue;
-            if (!visited[nx][ny])
-            {
-                if (map[nx][ny] == 0)
-                {
-                    visited[nx][ny] = true;
-                    q.push({ nx,ny });
-                }
-                if (map[nx][ny] == 3)
-                    return true;
-            }
-        }
-    }
-    return false;
-}
-
-int main(int argc, char** argv)
-{
-    int test_case;
-    int T;
-
-
-    for (test_case = 1; test_case <= 10; ++test_case)
-    {
-        cin >> T;
-        cout << "#" << T << " ";
-
-        int x; int y;
-        for (int i = 0; i < 100; i++)
-        {
-            string str;
-            cin >> str;
-            for (int j = 0; j < str.length(); j++)
-            {
-                map[i][j] = str[j] - '0';
-                visited[i][j] = false;
-
-                if (str[j] - '0' == 2)
-                {
-                    x = i;
-                    y = j;
+                if (nx < 0 || ny < 0 || nx >= 100 || ny >= 100) continue;
+                if (!visited[nx][ny]) {
+                    if (map[nx][ny] == 0) {
+                        visited[nx][ny] = true;
+                        q.offer(new int[]{nx, ny});
+                    }
+                    if (map[nx][ny] == 3) {
+                        return true;
+                    }
                 }
             }
         }
-
-        if (bfs(x, y)) cout << "1\n";
-        else cout << "0\n";
+        return false;
     }
-    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int T = sc.nextInt();
+            System.out.print("#" + T + " ");
+
+            int x = 0, y = 0;
+            for (int i = 0; i < 100; i++) {
+                String str = sc.next();
+                for (int j = 0; j < str.length(); j++) {
+                    map[i][j] = str.charAt(j) - '0';
+                    visited[i][j] = false;
+
+                    if (map[i][j] == 2) {
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+
+            if (bfs(x, y)) System.out.println("1");
+            else System.out.println("0");
+        }
+    }
 }
