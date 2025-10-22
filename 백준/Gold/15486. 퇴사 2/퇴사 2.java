074090684dd8e7ed8ad31811708cf72e2517231d
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2025-10-22 16:32:22 +0900

    [Gold V] 15486: 퇴사 2

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -7,21 +7,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] time = new int[n + 1];
-        int[] arr = new int[n + 1];
-        int[] dp = new int[1600001];
+        int[] dp = new int[n + 1];
 
+        int prev = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int t = Integer.parseInt(st.nextToken());
             int p = Integer.parseInt(st.nextToken());
-            time[i] = t;
-            arr[i] = p;
+            dp[i] = Math.max(dp[i], prev);
+            prev = Math.max(dp[i], prev);
 
-            dp[t + i] = Math.max(p + dp[i], dp[t + i]);
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            if (t + i <= n) {
+                dp[t + i] = Math.max(p + dp[i], dp[t + i]);
+            }
         }
 
-        System.out.println(dp[n]);
+        System.out.println(Math.max(dp[n], prev));
     }
 }
